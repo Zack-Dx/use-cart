dfca81129ed295983484365b2c127fa5e3a7a374
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-07-07 00:22:53 +0530

    Error Handling added.

--- useCart.js
+++ useCart.js
@@ -2,24 +2,42 @@ import { useState, useEffect } from "react";
 
 const useCart = () => {
   const [cart, setCart] = useState([]);
-  // Add to cart
 
   function addToCart(item) {
-    const newCart = [...cart, item];
-    setCart(newCart);
-    localStorage.setItem("cart", JSON.stringify(newCart));
+    try {
+      const newCart = [...cart, item];
+      setCart(newCart);
+      localStorage.setItem("cart", JSON.stringify(newCart));
+    } catch (error) {
+      console.error("Error adding item to cart:", error);
+    }
   }
+
   function removeFromCart(item) {
-    const filteredItems = cart.filter((product) => product.name !== item.name);
-    setCart(filteredItems); // Asynchronously
-    localStorage.setItem("cart", JSON.stringify(filteredItems));
+    try {
+      const index = cart.findIndex((product) => product.name === item.name);
+      if (index !== -1) {
+        const updatedCart = [...cart];
+        updatedCart.splice(index, 1);
+        setCart(updatedCart);
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
+      }
+    } catch (error) {
+      console.error("Error removing item from cart:", error);
+    }
   }
+
   useEffect(() => {
-    const storedCart = JSON.parse(localStorage.getItem("cart"));
-    if (storedCart) {
-      setCart(storedCart);
+    try {
+      const storedCart = JSON.parse(localStorage.getItem("cart"));
+      if (storedCart) {
+        setCart(storedCart);
+      }
+    } catch (error) {
+      console.error("Error retrieving cart from localStorage:", error);
     }
   }, []);
+
   return [cart, addToCart, removeFromCart];
 };
 
